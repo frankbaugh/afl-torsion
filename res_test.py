d432bf1440868d1da43d12340d20528a6257ba48
Frank Baugh <francisbaughofficial@gmail.com>
2023-11-05 17:06:40 +0000

    sdfaf

--- res_test.py
+++ res_test.py
@@ -46,22 +46,24 @@ def get_filelist():
 
 wf=open('error_pdb_dihedrals.txt','w')
 
-filelist = get_filelist()[0:1]
-for file in tqdm(filelist):
-    protein_id = file.replace('_protein.pdb', '')
-    print(f'id: f{protein_id}')
-    filepath = os.path.join(PDB_DIR, protein_id, file)
-    print(filepath)
-    parser=PDBParser()
-    structure=parser.get_structure(protein_id, filepath)
-    residues_shengyu = []
-    residues_mine = structure.get_residues() # Is this the same as below?
-    for chain in list(structure[0].get_chains()):
-        residues_shengyu += list(chain.get_residues())
+filelist = get_filelist()
+file = filelist[0]
 
-print("My residues:")
-print(residues_mine)
+protein_id = file.replace('_protein.pdb', '')
+print(f'id: f{protein_id}')
+filepath = os.path.join(PDB_DIR, protein_id, file)
 
-print()
-print("shengyu residues")
-print(residues_shengyu)
+parser=PDBParser()
+structure=parser.get_structure(protein_id, filepath)
+residues_shengyu = []
+residues_mine = structure.get_residues() # Is this the same as below?
+for chain in list(structure[0].get_chains()):
+    residues_shengyu += list(chain.get_residues())
+
+residues_shengyu_ids = [resi.get_id() for resi in residues_shengyu]
+residues_mine_ids = [resi.get_id() for resi in residues_mine]
+
+if residues_shengyu_ids == residues_mine_ids:
+    print("The order and content of residues are the same.")
+else:
+    print("The order and content of residues are not the same.")
