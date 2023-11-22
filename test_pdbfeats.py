75e1e46307b9a0aa9f7fd2c81540e0048b24ed87
Frank Baugh <francisbaughofficial@gmail.com>
2023-11-22 14:12:29 +0000

    open

--- test_pdbfeats.py
+++ test_pdbfeats.py
@@ -1,6 +1,7 @@
 import sys
-sys.path.append('/home/fmvb2/torch_af_ligand/afl/')
-from data.features import pdb_feat_loader
+sys.path.append('/home/fmvb2/')
+import openfold
+from torch_af_ligand.afl.data.features import pdb_feat_loader
 
 PDB_DIR='/rds/project/rds-a1NGKrlJtrw/dyna_mol/v2020-other-PL/3sm2/'
 file = '3sm2_protein.pdb'
