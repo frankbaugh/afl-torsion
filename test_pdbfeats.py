2422d38eaeb4e395f0fcf5e1defbf57c3e86fba2
Frank Baugh <francisbaughofficial@gmail.com>
2023-11-22 14:25:56 +0000

    asda

--- test_pdbfeats.py
+++ test_pdbfeats.py
@@ -7,5 +7,5 @@ from torch_af_ligand.afl.data import features
 PDB_DIR='/rds/project/rds-a1NGKrlJtrw/dyna_mol/v2020-other-PL/3sm2/'
 file = '3sm2_protein.pdb'
 
-pdb_feats = features.pdb_feat_loader(PDB_DIR + file, chain_id=None, is_distillation=False)
+pdb_feats = features.pdb_feat_loader(PDB_DIR + file, chain_id=0, is_distillation=False)
 print(pdb_feats)
