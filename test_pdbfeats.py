3fa21cdd5c21158dfa42ce951460433bb427c48c
Frank Baugh <francisbaughofficial@gmail.com>
2023-11-22 13:46:56 +0000

    pdbfeattest

---
+++ test_pdbfeats.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('/home/torch_af_ligand/afl/data/')
+from features import pdb_feat_loader
+
+PDB_DIR='/rds/project/rds-a1NGKrlJtrw/dyna_mol/v2020-other-PL/3sm2/'
+file = '3sm2_protein.pdb'
+
+pdb_feats = pdb_feat_loader(PDB_DIR + file, chain_id=None, is_distillation=False)
+print(pdb_feats)
